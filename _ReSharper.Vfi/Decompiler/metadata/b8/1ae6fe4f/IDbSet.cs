45d051392cf540785b2c2af00f6e0e86c21d55e1
tâm hoàng <htam0602@gmail.com>
2021-11-27 09:11:26 +0700

    000 upload code

---
+++ _ReSharper.Vfi/Decompiler/metadata/b8/1ae6fe4f/IDbSet.cs
@@ -0,0 +1,25 @@
+﻿// Type: System.Data.Entity.IDbSet`1
+// Assembly: EntityFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+// Assembly location: D:\Visual Studio 2010\Projects\Vfi\Assemblies\EntityFramework.dll
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace System.Data.Entity
+{
+  [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Justification = "Casing is intentional")]
+  [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "Name is intentional")]
+  public interface IDbSet<TEntity> : IQueryable<TEntity>, IEnumerable<TEntity>, IQueryable, IEnumerable where TEntity : class
+  {
+    TEntity Find(params object[] keyValues);
+    TEntity Add(TEntity entity);
+    TEntity Remove(TEntity entity);
+    TEntity Attach(TEntity entity);
+    TEntity Create();
+    TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, TEntity;
+    ObservableCollection<TEntity> Local { get; }
+  }
+}
